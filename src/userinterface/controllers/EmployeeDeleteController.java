036161f294f929f1658c12a02de066eea715d2cb
AlishT <alish@bigmir.net>
2017-01-27 01:05:08 +0200

    admin interface files: fixed mistakes, add RootsDaoImp, add working buttons to Employee

---
+++ src/userinterface/controllers/EmployeeDeleteController.java
@@ -0,0 +1,28 @@
+package userinterface.controllers;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class EmployeeDeleteController {
+    private Stage dialogStage;
+    private boolean okClicked = false;
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    @FXML
+    public void handleCancelButton(){
+        dialogStage.close();
+    }
+
+    @FXML
+    public void handleOkDelete() {
+        okClicked = true;
+        dialogStage.close();
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+}
